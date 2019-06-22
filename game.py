4ea1d7f50fc6e99989f3a755f8d3770ff1d1156d
Ted Nguyen <tpnguyen1@scu.edu>
2019-06-22 16:47:40 -0700

    Added space_check and full_board_check helper functions

--- game.py
+++ game.py
@@ -1,15 +1,21 @@
 # This will start the game
 def start_game():
     # Initialize the board to be empty
-    main_board = ['']*10
+    main_board = [' ']*10
     display_board(main_board)
     player1_marker, player2_marker = player_input()
 
+    # If user is X, they start first
+    if player1_marker == 'X':
+        choice = input("Choose a position (1-9): ")
+        print(full_board_check(main_board))
+        #place_marker(main_board, player1_marker, choice)
+
 # Introduces the game and displays the board with numbers to show the positions
 def intro():
-    print("Welcome to Tic-Tac-Toe! Here is what the game board will look like:")
     test_board = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
     display_board(test_board)
+    print("Welcome to Tic-Tac-Toe! Here is what the game board will look like.")
     input("Press Enter to continue...")
 
 # Clears the screen
@@ -60,8 +66,24 @@ def player_input():
     return (p1marker, p2marker)
 
 # Places the marker on the board given their marker and position selected
-#def place_marker(board, marker, position:
+#def place_marker(board, marker, position):
+    pass
 
+# Checks whether the position in the board is open
+def space_check(board, position):
+    if board[position] != ' ':
+        print(f"This spot is already taken by {board[position]}")
+
+# Checks whether the board is full and returns a boolean value. True = full, false otherwise
+def full_board_check(board):
+    index = 0
+    while index < len(board):
+        if board[index] == ' ':
+            return False
+        else:
+            index += 1
+
+    return True
 
 # This is where the game begins!
 intro()
