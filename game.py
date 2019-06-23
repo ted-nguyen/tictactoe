921524f1fe001094c2a24412cbbf52928ea3d879
Ted Nguyen <tpnguyen1@scu.edu>
2019-06-23 00:40:32 -0700

    Added check_win function

--- game.py
+++ game.py
@@ -1,14 +1,15 @@
+# importing "random" for random operations
+import random
+import time
 # Needed for clear() function
 from os import system, name
 from time import sleep
-# importing "random" for random operations
-import random
 
 # This will start the game
 def start_game():
     # Initialize the board to be empty
     main_board = [' ']*10
-    main_board[0] = '#'
+    clear()
     display_board(main_board)
     player1_marker, player2_marker = player_input()
 
@@ -16,44 +17,62 @@ def start_game():
     if player1_marker == 'X':
         # Keep prompting the player until they pick an open spot
         while True:
-            p1spot = int(input("Player1 mark a spot (1-9): "))
+            p1spot = int(input("Your turn to mark a spot (1-9): "))
             # Check if their choice is available
             if space_check(main_board, p1spot):
                 place_marker(main_board, player1_marker, p1spot)
                 display_board(main_board)
                 break
             else:
-                print(f"{p1spot} is already taken by {main_board[p1spot - 1]}. Look at the board for available spots.")
-
+                print(f"{p1spot} is already taken. Look at the board for available spots.")
+        # Keep assigning a random number until an available spot is chosen for the computer
         while True:
             # Assign p2spot to a random number between 1-9
             p2spot = random.randrange(1, 10)
             if space_check(main_board, p2spot):
+                # Wait 1.5 seconds before updating the board
+                time.sleep(1.5)
+                print(f"The computer has placed their marker at position {p2spot}")
                 place_marker(main_board, player2_marker, p2spot)
                 display_board(main_board)
                 break
             else:
-                # Keep assigning a random number until an available spot is chosen
                 p2spot = random.randrange(1,10)
 
     # Else, the user is O and the computer will go first
-    '''else:
+    else:
+        # Keep assigning a random number until an available spot is chosen for the computer
         while True:
-            choice = int(input("Player1 mark a spot (1-9): "))
-            # Check if their choice is available
-            if space_check(main_board, choice):
-                place_marker(main_board, player1_marker, choice)
+            # Assign p2spot to a random number between 1-9
+            p2spot = random.randrange(1, 10)
+            if space_check(main_board, p2spot):
+                print(f"The computer has placed their marker at {p2spot}")
+                place_marker(main_board, player2_marker, p2spot)
                 display_board(main_board)
-                break'''
+                break
+            else:
+                p2spot = random.randrange(1,10)
+
+        # Keep prompting the player until they pick an open spot
+        while True:
+            p1spot = int(input("Your turn to mark a spot (1-9): "))
+            # Check if their choice is available
+            if space_check(main_board, p1spot):
+                place_marker(main_board, player1_marker, p1spot)
+                display_board(main_board)
+                break
+            else:
+                print(f"{p1spot} is already taken. Look at the board for available spots.")
 
 # Introduces the game and displays the board with numbers to show the positions
 def intro():
+    clear()
     test_board = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
     display_board(test_board)
     print("Welcome to Tic-Tac-Toe! Here is what the game board will look like.")
     input("Press Enter to continue...")
 
-# Clears the screen
+# Clears the terminal screen
 def clear():
     # For windows
     if name == 'nt':
@@ -64,7 +83,6 @@ def clear():
 
 # Displays the tic-tac-toe board
 def display_board(board):
-    #clear()
     print("   |   |   ")
     print(' ' + board[0] + ' ' + '|' + ' ' + board[1] + ' ' + '|' + ' ' + board[2] + ' ')
     print("   |   |   ")
@@ -124,6 +142,25 @@ def full_board_check(board):
 # Places the player's marker at the given position on the baord
 def place_marker(board, marker, position):
     board[position - 1] = marker
+
+# Checks to see if a mark has three in a row (horizontal, vertical, or diagonal) to win the game and returns a boolean value
+def win_check(board, mark):
+    # Check if there was three in a row horizontally
+    for i in range(0, 9, 3):
+        if board[i] == board[i + 1] == board[i + 2] == mark:
+            return True
+    # Check if there was three in a row vertically
+    for i in range (0, 3):
+        if board[i] == board[i + 3] == board[i + 6] == mark:
+            return True
+    # Check first diagonal
+    if board[0] == board[4] == board[8] == mark:
+        return True
+    # Check second diagonal
+    if board[2] == board[4] == board[6] == mark:
+        return True
+
+    return False
 
 # This is where the game begins!
 intro()
